caa3b61267e4405c6140455bd1da78f856b5bfb7
WillowPunch <danek2002@gmail.com>
2024-12-01 20:43:33 +0300

    PARSER

--- NoAddressProcessor/NoAddressProcessor.cpp
+++ NoAddressProcessor/NoAddressProcessor.cpp
@@ -2,6 +2,9 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
 
 class Memory {
 private: 
@@ -188,7 +191,7 @@ void emulationStep(Stack stack, Register flag, Register command, Register counte
     system("pause");
 }
 
-int CPU()
+int CPU(Memory addedInstr, int mode)
 {
     Memory dataMemory;
     Memory instMemory;
@@ -203,34 +206,40 @@ int CPU()
     dataMemory.write(13, 2);
     dataMemory.write(14, 1);
     dataMemory.write(15, 0);
-    instMemory.write(0, 0b0000); //PUSH 6 - THE SIZE OF ARRAY
-    instMemory.write(1, 6);
-    instMemory.write(2, 0b0000); //PUSH 10 - THE ADDRESS OF FIRST ELEMENT OF ARRAY
-    instMemory.write(3, 10);
-    instMemory.write(4, 0b1100); // DUP 10
-    instMemory.write(5, 0b1010); // ROL
-    instMemory.write(6, 0b0011); // ADD TO FIND THE END OF ARRAY
-    instMemory.write(7, 0b1000); // SWAP TO PUT ADDRESS 10 ON TOP
-    instMemory.write(8, 0b1100); // DUP 10 TO HAVE BOTH ADDRESS AND POINTER
-    instMemory.write(9, 0b0010); // READ ADDRESS 10
-    instMemory.write(10, 0b1010); // ROL TO PUT POINTER ON TOP AND ADDRESS OF LAST PRE-TOP
-    instMemory.write(11, 0b1110); // INC TO NEXT ADDRESS
-    instMemory.write(12, 0b1011); // CMP NEXT AND LAST ADDRESSES
-    instMemory.write(13, 0b1101); // IF TRUE
-    instMemory.write(14, 24); // JZ TO OUTPUT
-    instMemory.write(15, 0b1001); // ROR AND SWP TO PUT NEXT ADDRESS ON TOP, SUM ON PRE-TOP
-    instMemory.write(16, 0b1000);
-    instMemory.write(17, 0b1100); // DUP NEXT ADDRESS
-    instMemory.write(18, 0b0010); // READ ADDRESS
-    instMemory.write(19, 0b1001); // ROR TO PUT TWO VALUES ON TOP
-    instMemory.write(20, 0b0011); // ADD NUMBERS
-    instMemory.write(21, 0b0000); // PUSH THE INSTRUCTION 10 IN STACK
-    instMemory.write(22, 10);
-    instMemory.write(23, 0b0110); // LDC. COUNTER IS SET TO 10. LOOP
-    instMemory.write(24, 0b1001); // ROR TO PUT SUM ON TOP
-    instMemory.write(25, 0b1111); // OUTPUT SUM
-    instMemory.write(26, 0b0111); // FULL STOP
-
+    if (mode == 1)
+    {
+        instMemory.write(0, 0b0000); //PUSH 6 - THE SIZE OF ARRAY
+        instMemory.write(1, 6);
+        instMemory.write(2, 0b0000); //PUSH 10 - THE ADDRESS OF FIRST ELEMENT OF ARRAY
+        instMemory.write(3, 10);
+        instMemory.write(4, 0b1100); // DUP 10
+        instMemory.write(5, 0b1010); // ROL
+        instMemory.write(6, 0b0011); // ADD TO FIND THE END OF ARRAY
+        instMemory.write(7, 0b1000); // SWAP TO PUT ADDRESS 10 ON TOP
+        instMemory.write(8, 0b1100); // DUP 10 TO HAVE BOTH ADDRESS AND POINTER
+        instMemory.write(9, 0b0010); // READ ADDRESS 10
+        instMemory.write(10, 0b1010); // ROL TO PUT POINTER ON TOP AND ADDRESS OF LAST PRE-TOP
+        instMemory.write(11, 0b1110); // INC TO NEXT ADDRESS
+        instMemory.write(12, 0b1011); // CMP NEXT AND LAST ADDRESSES
+        instMemory.write(13, 0b1101); // IF TRUE
+        instMemory.write(14, 24); // JZ TO OUTPUT
+        instMemory.write(15, 0b1001); // ROR AND SWP TO PUT NEXT ADDRESS ON TOP, SUM ON PRE-TOP
+        instMemory.write(16, 0b1000);
+        instMemory.write(17, 0b1100); // DUP NEXT ADDRESS
+        instMemory.write(18, 0b0010); // READ ADDRESS
+        instMemory.write(19, 0b1001); // ROR TO PUT TWO VALUES ON TOP
+        instMemory.write(20, 0b0011); // ADD NUMBERS
+        instMemory.write(21, 0b0000); // PUSH THE INSTRUCTION 10 IN STACK
+        instMemory.write(22, 10);
+        instMemory.write(23, 0b0110); // LDC. COUNTER IS SET TO 10. LOOP
+        instMemory.write(24, 0b1001); // ROR TO PUT SUM ON TOP
+        instMemory.write(25, 0b1111); // OUTPUT SUM
+        instMemory.write(26, 0b0111); // FULL STOP
+    }
+    else
+    {
+        instMemory = addedInstr;
+    }
     while (noStop)
     {
         comRegister.setValue(instMemory.read(pc.getValue()));
@@ -301,16 +310,68 @@ int CPU()
     return stackRegisters.peek();
 }
 
-void parser()
+std::map<std::string, int> mapping()
 {
+    std::map<std::string, int> mapper;
+    mapper["PUSH"] =  0b0000;
+    mapper["POP"]  =  0b0001;
+    mapper["READ"] =  0b0010;
+    mapper["ADD"]  =  0b0011;
+    mapper["SUB"]  =  0b0100;
+    mapper["WRITE"]=  0b0101;
+    mapper["LDC"]  =  0b0110;
+    mapper["FST"]  =  0b0111;
+    mapper["SWAP"] =  0b1000;
+    mapper["ROR"]  =  0b1001;
+    mapper["ROL"]  =  0b1010;
+    mapper["CMP"]  =  0b1011;
+    mapper["DUP"]  =  0b1100;
+    mapper["JZ"]   =  0b1101;
+    mapper["INC"]  =  0b1110;
+    mapper["OUT"]  =  0b1111;
+    return mapper;
+}
 
+Memory parser()
+{
+    std::ifstream f("input.txt");
+    Memory instructions;
+    std::map<std::string, int> mapper;
+    mapper = mapping();
+    int instrCnt = 0;
+    bool numberWaiter = false;
+    // String variable to store the read data
+    std::string s;
+    while (getline(f, s))
+    {
+            if (numberWaiter == true)
+            {
+                instructions.write(instrCnt, std::stoi(s));
+                numberWaiter = false;
+            }
+            else
+            {
+                instructions.write(instrCnt, mapper[s]);
+                if (mapper[s] == 0b0000 || mapper[s] == 0b1101) // PUSH OR JZ
+                {
+                    numberWaiter = true;
+                }
+            }
+            instrCnt += 1;
+    }
+    f.close();
+    return instructions;
 }
 
 
 int main()
 {
-    if (CPU() == 15)
+    Memory parsedMem = parser();
+    std::cout << "PRESS 1 FOR DIRECT MODE, 2 FOR PARSED MODE." << std::endl;
+    int mode;
+    std::cin >> mode;
+    if (CPU(parsedMem, mode) == 15)
     {
-        std::cout << "Test passed";
+       std::cout << "Test passed";
     }
 }
