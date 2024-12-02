306c2419c37396359d019204b144117fce1041a7
WillowPunch <74188076+WillowPunch@users.noreply.github.com>
2024-12-02 19:35:47 +0300

    АРА, ИСПРАВЛЯЙ

--- NoAddressProcessor/NoAddressProcessor.cpp
+++ NoAddressProcessor/NoAddressProcessor.cpp
@@ -7,7 +7,7 @@
 #include <map>
 
 class Memory {
-private: 
+private:
     short data[1024] = {};
 public:
     void write(short address, short value)
@@ -48,13 +48,13 @@ public:
     }
 };
 
-class Stack
+class DataMemory
 {
-    
+
 public:
-    Register stack[8];
-    int n = 8;
-    int top = -1;
+    short stack[1024] = {};
+    int n = 1024;
+    int top = 1023;
     int readTop()
     {
         return top;
@@ -66,69 +66,77 @@ public:
     void push(short val)
     {
         int tempTop = readTop();
-        if (tempTop >= n - 1)
+        if (tempTop <= 0)
             return;
         else {
-            tempTop++;
+            tempTop--;
             setTop(tempTop);
-            stack[tempTop].setValue(val);
+            stack[tempTop] = (val);
         }
     }
     short pop() {
         int tempTop = readTop();
-        if (tempTop <= -1)
+        if (tempTop >= 1024)
             return -1;
         else {
-            tempTop--;
+            tempTop++;
             setTop(tempTop);
-            return stack[tempTop+1].getValue();
+            return stack[tempTop - 1];
         }
     }
     short peek() {
-        if (top <= -1)
+        if (top >= 1024)
             return -1;
         else
         {
-            return stack[top].getValue();
+            return stack[top];
         }
     }
     void ror()
     {
-        short tempVar = stack[top].getValue();
-        stack[top].setValue(stack[top - 2].getValue());
-        stack[top - 2].setValue(stack[top - 1].getValue());
-        stack[top - 1].setValue(tempVar);
-        
+        short tempVar = stack[top];
+        stack[top] = stack[top + 2];
+        stack[top + 2]=stack[top + 1];
+        stack[top + 1] = tempVar;
+
     }
     void rol()
     {
-        short tempVar = stack[top].getValue();
-        stack[top].setValue(stack[top - 1].getValue());
-        stack[top - 1].setValue(stack[top - 2].getValue());
-        stack[top - 2].setValue(tempVar);
+        short tempVar = stack[top];
+        stack[top] = stack[top + 1];
+        stack[top + 1] = stack[top + 2];
+        stack[top + 2] = tempVar;
     }
     void swap()
     {
-        short tempVar = stack[top].getValue();
-        stack[top].setValue(stack[top - 1].getValue());
-        stack[top - 1].setValue(tempVar);
+        short tempVar = stack[top];
+        stack[top] = stack[top + 1];
+        stack[top + 1] = tempVar;
     }
     bool compare()
     {
-        return stack[top].getValue() == stack[top - 1].getValue();
+        return stack[top] == stack[top + 1];
     }
     void increment(short value)
     {
-        stack[top].incrementBy(value);
+        stack[top]++;
+    }
+    void write(short address, short value)
+    {
+        stack[address] = value;
+    }
+    short read(short address)
+    {
+        return stack[address];
     }
 };
 
-void emulationStep(Stack stack, Register flag, Register command, Register counter, Memory instMemory, Memory dataMemory)
+void emulationStep(DataMemory stack, Register flag, Register command, Register counter, Memory instMemory)
 {
     std::cout << "Stack: ";
-    for (int i = 0; i < 8; i++)
+    for (int i = 1023; i > 1000; i--)
     {
-        std::cout << stack.stack[i].getValue() << " ";
+        std::cout << stack.stack[i] << " ";
         if (i == stack.readTop())
         {
             std::cout << "] ";
@@ -145,16 +153,16 @@ void emulationStep(Stack stack, Register flag, Register command, Register counte
         std::cout << "POP " << stack.peek();
         break;
     case 0b0010:
-        std::cout << "READ " << stack.peek() << " TO PUSH " << dataMemory.read(stack.peek());
+        std::cout << "READ " << stack.peek() << " TO PUSH " << stack.read(stack.peek());
         break;
     case 0b0011:
-        std::cout << "ADD " << stack.peek() << " AND " << stack.stack[stack.readTop() - 1].getValue();
+        std::cout << "ADD " << stack.peek() << " AND " << stack.stack[stack.readTop() + 1];
         break;
     case 0b0100:
-        std::cout << "SUB " << stack.peek() << " AND " << stack.stack[stack.readTop() - 1].getValue();
+        std::cout << "SUB " << stack.peek() << " AND " << stack.stack[stack.readTop() + 1];
         break;
     case 0b0101:
-        std::cout << "WRITE VALUE " << stack.stack[stack.readTop() - 1].getValue() << " TO ADDRESS " << stack.peek();
+        std::cout << "WRITE VALUE " << stack.stack[stack.readTop() + 1] << " TO ADDRESS " << stack.peek();
         break;
     case 0b0110:
         std::cout << "LDC: JUMP TO INSTRUCTION " << stack.peek();
@@ -163,16 +171,16 @@ void emulationStep(Stack stack, Register flag, Register command, Register counte
         std::cout << "FULL STOP";
         break;
     case 0b1000:
-        std::cout << "SWAP " << stack.peek() << " AND " << stack.stack[stack.readTop() - 1].getValue();
+        std::cout << "SWAP " << stack.peek() << " AND " << stack.stack[stack.readTop() + 1];
         break;
     case 0b1001:
-        std::cout << "ROR: ROTATE " << stack.peek() << ", " << stack.stack[stack.readTop() - 1].getValue() << " AND " << stack.stack[stack.readTop() - 2].getValue() << " TO THE RIGHT";
+        std::cout << "ROR: ROTATE " << stack.peek() << ", " << stack.stack[stack.readTop() + 1] << " AND " << stack.stack[stack.readTop() + 2] << " TO THE RIGHT";
         break;
     case 0b1010:
-        std::cout << "ROL: ROTATE " << stack.peek() << ", " << stack.stack[stack.readTop() - 1].getValue() << " AND " << stack.stack[stack.readTop() - 2].getValue() << " TO THE LEFT";
+        std::cout << "ROL: ROTATE " << stack.peek() << ", " << stack.stack[stack.readTop() + 1] << " AND " << stack.stack[stack.readTop() + 2] << " TO THE LEFT";
         break;
     case 0b1011:
-        std::cout << "CMP " << stack.peek() << " AND " << stack.stack[stack.readTop() - 1].getValue();
+        std::cout << "CMP " << stack.peek() << " AND " << stack.stack[stack.readTop() + 1];
         break;
     case 0b1100:
         std::cout << "DUP " << stack.peek();
@@ -193,48 +201,49 @@ void emulationStep(Stack stack, Register flag, Register command, Register counte
 
 int CPU(Memory addedInstr, int mode)
 {
-    Memory dataMemory;
     Memory instMemory;
     Register pc;
     Register flags;
-    Stack stackRegisters;
+    DataMemory stackRegisters;
     Register comRegister;
     bool noStop = true;
-    dataMemory.write(10, 5);
-    dataMemory.write(11, 4);
-    dataMemory.write(12, 3);
-    dataMemory.write(13, 2);
-    dataMemory.write(14, 1);
-    dataMemory.write(15, 0);
+    stackRegisters.write(0, 5);
+    stackRegisters.write(1, 4);
+    stackRegisters.write(2, 3);
+    stackRegisters.write(3, 2);
+    stackRegisters.write(4, 1);
+    stackRegisters.write(5, 0);
     if (mode == 1)
     {
-        instMemory.write(0, 0b0000); //PUSH 6 - THE SIZE OF ARRAY
-        instMemory.write(1, 6);
-        instMemory.write(2, 0b0000); //PUSH 10 - THE ADDRESS OF FIRST ELEMENT OF ARRAY
-        instMemory.write(3, 10);
-        instMemory.write(4, 0b1100); // DUP 10
-        instMemory.write(5, 0b1010); // ROL
-        instMemory.write(6, 0b0011); // ADD TO FIND THE END OF ARRAY
-        instMemory.write(7, 0b1000); // SWAP TO PUT ADDRESS 10 ON TOP
-        instMemory.write(8, 0b1100); // DUP 10 TO HAVE BOTH ADDRESS AND POINTER
-        instMemory.write(9, 0b0010); // READ ADDRESS 10
-        instMemory.write(10, 0b1010); // ROL TO PUT POINTER ON TOP AND ADDRESS OF LAST PRE-TOP
-        instMemory.write(11, 0b1110); // INC TO NEXT ADDRESS
-        instMemory.write(12, 0b1011); // CMP NEXT AND LAST ADDRESSES
-        instMemory.write(13, 0b1101); // IF TRUE
-        instMemory.write(14, 24); // JZ TO OUTPUT
-        instMemory.write(15, 0b1001); // ROR AND SWP TO PUT NEXT ADDRESS ON TOP, SUM ON PRE-TOP
-        instMemory.write(16, 0b1000);
-        instMemory.write(17, 0b1100); // DUP NEXT ADDRESS
-        instMemory.write(18, 0b0010); // READ ADDRESS
-        instMemory.write(19, 0b1001); // ROR TO PUT TWO VALUES ON TOP
-        instMemory.write(20, 0b0011); // ADD NUMBERS
-        instMemory.write(21, 0b0000); // PUSH THE INSTRUCTION 10 IN STACK
-        instMemory.write(22, 10);
-        instMemory.write(23, 0b0110); // LDC. COUNTER IS SET TO 10. LOOP
-        instMemory.write(24, 0b1001); // ROR TO PUT SUM ON TOP
-        instMemory.write(25, 0b1111); // OUTPUT SUM
-        instMemory.write(26, 0b0111); // FULL STOP
+        instMemory.write(0, 0b0000); //PUSH 0 - THE ADDRESS OF THE SIZE OF ARRAY
+        instMemory.write(1, 0);
+        instMemory.write(2, 0b0010); // READ 0 
+        instMemory.write(3, 0b0000); //PUSH 1 - THE ADDRESS OF FIRST ELEMENT OF ARRAY
+        instMemory.write(4, 1);
+        instMemory.write(5, 0b1100); // DUP 1
+        instMemory.write(6, 0b1010); // ROL
+        instMemory.write(7, 0b0011); // ADD TO FIND THE END OF ARRAY
+        instMemory.write(8, 0b1000); // SWAP TO PUT ADDRESS 10 ON TOP
+        instMemory.write(9, 0b1100); // DUP 10 TO HAVE BOTH ADDRESS AND POINTER
+        instMemory.write(10, 0b0010); // READ ADDRESS 1
+        instMemory.write(11, 0b1010); // ROL TO PUT POINTER ON TOP AND ADDRESS OF LAST PRE-TOP
+        instMemory.write(12, 0b1110); // INC TO NEXT ADDRESS
+        instMemory.write(13, 0b1011); // CMP NEXT AND LAST ADDRESSES
+        instMemory.write(14, 0b0000); // PUSH INSTRUCTION ADDRESS 26
+        instMemory.write(15, 26);
+        instMemory.write(16, 0b1101); // IF TRUE JUMP TO OUTPUT
+        instMemory.write(17, 0b1001); // ROR AND SWP TO PUT NEXT ADDRESS ON TOP, SUM ON PRE-TOP
+        instMemory.write(18, 0b1000);
+        instMemory.write(19, 0b1100); // DUP NEXT ADDRESS
+        instMemory.write(20, 0b0010); // READ ADDRESS
+        instMemory.write(21, 0b1001); // ROR TO PUT TWO VALUES ON TOP
+        instMemory.write(22, 0b0011); // ADD NUMBERS
+        instMemory.write(23, 0b0000); // PUSH THE INSTRUCTION 10 IN STACK
+        instMemory.write(24, 11);
+        instMemory.write(25, 0b0110); // LDC. COUNTER IS SET TO 10. LOOP
+        instMemory.write(26, 0b1001); // ROR TO PUT SUM ON TOP
+        instMemory.write(27, 0b1111); // OUTPUT SUM
+        instMemory.write(28, 0b0111); // FULL STOP
     }
     else
     {
@@ -243,7 +252,7 @@ int CPU(Memory addedInstr, int mode)
     while (noStop)
     {
         comRegister.setValue(instMemory.read(pc.getValue()));
-        emulationStep(stackRegisters, flags, comRegister, pc, instMemory, dataMemory);
+        emulationStep(stackRegisters, flags, comRegister, pc, instMemory);
         switch (comRegister.getValue())
         {
         case 0b0000: // PUSH
@@ -254,7 +263,7 @@ int CPU(Memory addedInstr, int mode)
             stackRegisters.pop();
             break;
         case 0b0010: // READ
-            stackRegisters.push(dataMemory.read(stackRegisters.pop()));
+            stackRegisters.push(stackRegisters.read(stackRegisters.pop()));
             break;
         case 0b0011: // ADD
             stackRegisters.push(stackRegisters.pop() + stackRegisters.pop());
@@ -290,12 +299,13 @@ int CPU(Memory addedInstr, int mode)
             stackRegisters.push(stackRegisters.peek());
             break;
         case 0b1101: // JZ
-            pc.incrementBy(1);
             if (flags.getValue() == 1)
             {
                 flags.setValue(0);
-                stackRegisters.push(instMemory.read(pc.getValue()));
                 pc.setValue(stackRegisters.pop() - 1);
+            }
+            else {
+                stackRegisters.pop();
             }
             break;
         case 0b1110: // INC
@@ -313,22 +323,22 @@ int CPU(Memory addedInstr, int mode)
 std::map<std::string, int> mapping()
 {
     std::map<std::string, int> mapper;
-    mapper["PUSH"] =  0b0000;
-    mapper["POP"]  =  0b0001;
-    mapper["READ"] =  0b0010;
-    mapper["ADD"]  =  0b0011;
-    mapper["SUB"]  =  0b0100;
-    mapper["WRITE"]=  0b0101;
-    mapper["LDC"]  =  0b0110;
-    mapper["FST"]  =  0b0111;
-    mapper["SWAP"] =  0b1000;
-    mapper["ROR"]  =  0b1001;
-    mapper["ROL"]  =  0b1010;
-    mapper["CMP"]  =  0b1011;
-    mapper["DUP"]  =  0b1100;
-    mapper["JZ"]   =  0b1101;
-    mapper["INC"]  =  0b1110;
-    mapper["OUT"]  =  0b1111;
+    mapper["PUSH"] = 0b0000;
+    mapper["POP"] = 0b0001;
+    mapper["READ"] = 0b0010;
+    mapper["ADD"] = 0b0011;
+    mapper["SUB"] = 0b0100;
+    mapper["WRITE"] = 0b0101;
+    mapper["LDC"] = 0b0110;
+    mapper["FST"] = 0b0111;
+    mapper["SWAP"] = 0b1000;
+    mapper["ROR"] = 0b1001;
+    mapper["ROL"] = 0b1010;
+    mapper["CMP"] = 0b1011;
+    mapper["DUP"] = 0b1100;
+    mapper["JZ"] = 0b1101;
+    mapper["INC"] = 0b1110;
+    mapper["OUT"] = 0b1111;
     return mapper;
 }
 
@@ -344,20 +354,20 @@ Memory parser()
     std::string s;
     while (getline(f, s))
     {
-            if (numberWaiter == true)
+        if (numberWaiter == true)
+        {
+            instructions.write(instrCnt, std::stoi(s));
+            numberWaiter = false;
+        }
+        else
+        {
+            instructions.write(instrCnt, mapper[s]);
+            if (mapper[s] == 0b0000) // PUSH OR JZ
             {
-                instructions.write(instrCnt, std::stoi(s));
-                numberWaiter = false;
+                numberWaiter = true;
             }
-            else
-            {
-                instructions.write(instrCnt, mapper[s]);
-                if (mapper[s] == 0b0000 || mapper[s] == 0b1101) // PUSH OR JZ
-                {
-                    numberWaiter = true;
-                }
-            }
-            instrCnt += 1;
+        }
+        instrCnt += 1;
     }
     f.close();
     return instructions;
@@ -370,8 +380,8 @@ int main()
     std::cout << "PRESS 1 FOR DIRECT MODE, 2 FOR PARSED MODE." << std::endl;
     int mode;
     std::cin >> mode;
-    if (CPU(parsedMem, mode) == 15)
+    if (CPU(parsedMem, mode) == 10)
     {
-       std::cout << "Test passed";
+        std::cout << "Test passed";
     }
 }
